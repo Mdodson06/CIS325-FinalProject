0ee95b9e08a5739d01292014738e7737f5772add
Mdodson06 <mariadodson8@gmail.com>
2025-04-21 20:59:12 -0500

    Fix found

--- client/src/Internal.jsx
+++ client/src/Internal.jsx
@@ -43,10 +43,66 @@ function Internal() {
     urlParams.append("tableName",searchType);
     urlParams.append("name",searchTerm)
 
+    var searchTerm = new FormData(document.getElementById('generalSearchForm')).get("search");
+    //var urlData = Object.fromEntries(formData);
+    var urlParams = new URLSearchParams();
+    
+    urlParams.append("tableName",searchType);
+    urlParams.append("name",searchTerm)
+    const url = `http://localhost:8080/api/creation?${urlParams}`;
+
+    const response = await fetch(url, {
+      method: 'GET',
+      headers: {
+      'Content-Type': 'application/json'
+      },
+      })
+      .catch(error => {
+        alert(error);
+      });
+    
+      var result = await response.json()
+      .then(json => {  
+        const jsonArray = Object.values(json);
+        var htmlText = '<table> <tr>';
+        alert("Response: " + JSON.stringify(json));
+        
+        if(jsonArray.length <= 0) {
+            alert("Empty needed: " + JSON.stringify(json))
+            //hardcoded 
+            htmlText += '<th>name</th>';
+            htmlText += '<th>type</th>';
+            htmlText += '</tr>'
+            htmlText += "</table>"
+
+        } else {
+            const obs = Object.keys(jsonArray[0]);
+            for (let i = 0; i < obs.length; i++) {
+                htmlText += '<th>' + obs[i] + '</th>';
+            }
+            htmlText += '</tr>'
+            for (let i = 0; i < jsonArray.length; i++) {
+                var row = Object.values(jsonArray[i]);
+                htmlText += '<tr>'
+                for (let j = 0; j < row.length; j++) {
+                    htmlText += '<td>' + row[j] + '</td>';                        
+                }
+                htmlText +="</tr>"
+            }
+            htmlText += "</table>"
+        }
+        document.getElementById("searchResults").innerHTML = htmlText;
+      }).catch(error => {alert("Second error: "); alert(error)});
+
+
+
+
+
+/* 
     //Blank form should just return a selection of all fields as handled currently
     /* if(formData.get('search').length <= 0) {
       alert("All fields must be filled");
-    } */
+    } * /
     const url = `http://localhost:8080/api/creation?${urlParams}`;
     //const url = `http://localhost:8080/api/creation?tableName=${searchType}`;
     const response = await fetch(url, {
@@ -100,7 +156,7 @@ function Internal() {
     })
       .catch(error => {
         alert(error);
-      });
+      }); */
   }
 
   if(pageType == "generalSearch") {
